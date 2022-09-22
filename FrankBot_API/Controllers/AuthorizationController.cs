d9c173ecbf7f596ba8901187d48fa087fc34590b
Салтыков Виктор <saltyckov.vitya@yandex.ru>
2022-09-22 23:44:10 +0300

    Add authorization

---
+++ FrankBot_API/Controllers/AuthorizationController.cs
@@ -0,0 +1,53 @@
+﻿using FrankBot_API.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+
+namespace FrankBot_API.Controllers
+{
+    [Route("/[controller]/[action]")]
+    [ApiController]
+    public class AuthorizationController: ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public AuthorizationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Token([FromBody] LoginModel data)
+        {
+            if (data.UserName == "Vitya" && data.Password == "111")
+            {
+                var tokenString = GenerateJwtToken(data.UserName);
+                return Ok(new { Token = tokenString, Message = "Success" });
+            }
+            return BadRequest("Please pass the valid Username and Password");
+        }
+
+        private string GenerateJwtToken(string userName)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[] { new Claim("id", userName) }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ FrankBot_API/Models/LoginModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrankBot_API.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
